cd600c470865ed3c30b5b5f766f756be978d0092
Ulaş Can Zorer <ulascanzorer@gmail.com>
2023-10-17 13:59:23 +0200

    Added a bunch of stuff.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# DarkSouls1WeaponGenerator
-A GAN model trying to create new Dark Souls 1 weapons.
+# DarkSoulsWeaponGenerator
+A GAN model trying to create new Dark Souls weapons (currently using Dark Souls 1 and Dark Souls 2 weapons as input).
+
+Idea inspired by: 
+https://www.reddit.com/r/darksouls3/comments/6pxu7a/i_trained_a_neural_network_to_hallucinate_ds3/ by user normally_i_lurk
+
+
+Architecture inspired by:
+1) "Unsupervised representation learning with deep convolutional generative adversarial networks" by Alec Radford, Luke Metz and Soumith Chintala https://arxiv.org/abs/1511.06434
+2) https://github.com/Ksuryateja/DCGAN-CIFAR10-pytorch/tree/master
--- dataset.py
+++ dataset.py
@@ -7,16 +7,28 @@ from PIL import Image
 from torchvision.transforms.functional import pil_to_tensor
 import numpy as np
 import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
 
 class WeaponsDataset(Dataset):
 
-    def __init__(self):
+    def __init__(self, transform=None):
         self.samples = []
+
+        self.transform = transform
         img_folder_path = "./data/Dark_Souls_1_Weapons"
         image_paths = [join(img_folder_path, f) for f in listdir(img_folder_path)]
         for path in image_paths:
             img = Image.open(path)
             image_tensor = pil_to_tensor(img)
+            image_tensor = image_tensor / 255
+
+            if self.transform is not None:
+                image_tensor = self.transform(image_tensor)
+
+            # image_tensor = F.pad(input=image_tensor, pad=(0, 10, 0, 0, 0, 0), mode="constant", value=-1)
+
             self.samples.append(image_tensor)
 
 
@@ -27,10 +39,24 @@ class WeaponsDataset(Dataset):
         return self.samples[idx]
 
 if __name__ == "__main__":
-    dataset = WeaponsDataset()
+    transform = transforms.Compose(
+        [transforms.Resize((64, 64)),
+        transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))]
+        )
+    dataset = WeaponsDataset(transform)
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
     examples = iter(dataloader)
-    sample = next(examples).view(4, 90, 80)
+    samples = next(examples)
+    sample = samples[0]
+
+    # print(sample.shape, sample.dtype)
+    # sample = sample / 255
+    
+    # sample = transform(sample)
+    # print(sample.dtype)
+    print(sample.shape)
+
     image = np.transpose(sample.detach(), (1, 2, 0))
+    image = image / 2 + 0.5
     plt.imshow(image)
     plt.show()
